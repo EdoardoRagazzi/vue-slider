e593feb29c581484e3ae2d965bf026863c0e39c5
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-22 20:09:51 +0200

    Slide image with active

--- js/script.js
+++ js/script.js
@@ -29,11 +29,31 @@ const app = new Vue(
         methods: {
             nextPhoto() {
                 (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
+                removeActiveClasses()
             },
             prevPhoto() {
                 (this.counter == 0) ? this.counter = this.counter = this.images.length - 1 : this.counter--;
+                removeActiveClasses()
             }
 
         }
 
     });
+
+/* ------------------------------ */
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+
+    })
+
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
