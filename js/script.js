4a1a5f0641c8b2fefa1339f5ec5c0b3b17eb675b
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-22 23:40:09 +0200

    PANEL ACTIVE

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-// const panels = document.querySelectorAll('.panel');
 
-// panels.forEach((panel) => {
-//     panel.addEventListener('click', () => {
-//         removeActiveClasses()
-//         panel.classList.add('active')
-//     })
-
-// })
-
-// function removeActiveClasses() {
-//     panels.forEach(panel => {
-//         panel.classList.remove('active')
-//     })
-// };
-
+//----------------VUE JS------------------//
 const app = new Vue(
     {
         el: "#app",
@@ -41,6 +27,7 @@ const app = new Vue(
     });
 
 /* ------------------------------ */
+
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
