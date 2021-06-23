55b0a792badb5524427b67b0e8cebc8fed91890b
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-23 10:58:36 +0200

    AddEventListener Keydown

--- js/script.js
+++ js/script.js
@@ -11,13 +11,12 @@ const app = new Vue(
                 "https://source.unsplash.com/random",
                 "https://source.unsplash.com/collection/random"
             ],
-
+            intervallo: '',
         },
         methods: {
             nextPhoto() {
                 (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
                 removeActiveClasses()
-
             },
             prevPhoto() {
                 (this.counter == 0) ? this.counter = this.counter = this.images.length - 1 : this.counter--;
@@ -25,30 +24,33 @@ const app = new Vue(
             },
             cambiaPhoto(indice) {
                 this.counter = indice;
+                clearInterval(this.intervallo);
             }
+        },
+        // MOUNTED TIMING
+        mounted() {
+            // this.intervallo = setInterval(this.nextPhoto, 3000);
 
+            document.addEventListener("keydown", (element) => {
+                this.nextPhoto()
+                console.log(element.key)
+            });
         }
-
     }
 );
-
-
-
-
 
 /* ------------------------------ */
 
 const panels = document.querySelectorAll('.panel');
-
+// CICLO FOR EACH FOR EVERY PANELS
 panels.forEach((panel) => {
-
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 
 })
-
+// FUNCTIONS THAT REMOVE ACTIVE CLASSES
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
